9aa7df33472a963376797a57cd5926d629ca5609
Will <willbickham3@gmail.com>
2024-07-01 16:03:34 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import GameBoard from "./gameboard.js"
+import Ship from "./ship.js"
+import Player from "./player.js";
+import Game from "./game.js";
+
+
+// const player1 = new Player("player1")
+// const computer = new Player('computer')
+
+// player1.renderBoard()
+// computer.renderBoard()
+// player1.board.placeShip("Destroyer", 3, 5)
+// player1.renderBoard()
+
+const game = new Game("player1", "computer");
+game.startGame()
+
+// const game = new Game("Me", "Computer")
+// game.startGame()
