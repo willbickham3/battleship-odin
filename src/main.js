af1bb23e1e0d58d487316aadc98c6f7a5679b23f
Will <willbickham3@gmail.com>
2024-07-02 00:21:05 -0500

    Temp file to append main dom element

--- src/main.js
+++ src/main.js
@@ -1,19 +1,5 @@
-import GameBoard from "./gameboard.js"
-import Ship from "../ship.js"
-import Player from "./player.js";
-import Game from "./game.js";
-
-
-// const player1 = new Player("player1")
-// const computer = new Player('computer')
-
-// player1.renderBoard()
-// computer.renderBoard()
-// player1.board.placeShip("Destroyer", 3, 5)
-// player1.renderBoard()
-
-const game = new Game("player1", "computer");
-game.startGame()
-
-// const game = new Game("Me", "Computer")
-// game.startGame()
+export default function createMain() {
+    let main = document.createElement('main.root')
+    let body = document.querySelector('body')
+    body.append(main)
+}
