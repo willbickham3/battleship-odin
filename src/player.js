ed2a08536e7735cb8d32309efb548c38bffb6224
Will <willbickham3@gmail.com>
2024-07-21 16:46:11 -0500

    Populate the dom with ship images from battleship-assets

--- src/player.js
+++ src/player.js
@@ -1,5 +1,31 @@
 import GameBoard from "./gameboard.js";
 
+// carrier
+const requireCarrier = require.context("./battleship-assets/Carrier/Pieces", false, /\.png$/);
+const carrier = requireCarrier.keys().map(requireCarrier);
+let carrierIndex = 0
+
+// destroyer
+const requireDestroyer = require.context("./battleship-assets/Destroyer/Pieces", false, /\.png$/);
+const destroyer = requireDestroyer.keys().map(requireDestroyer);
+let destroyerIndex = 0
+
+// battleship
+const requireBattleship = require.context("./battleship-assets/Battleship/Pieces", false, /\.png$/);
+const battleship = requireBattleship.keys().map(requireBattleship);
+let battleshipIndex = 0
+
+// submarine
+const requireSubmarine = require.context("./battleship-assets/Submarine/Pieces", false, /\.png$/);
+const submarine = requireSubmarine.keys().map(requireSubmarine);
+let submarineIndex = 0
+
+// patrol boat
+
+const requirePatrolBoat = require.context("./battleship-assets/PatrolBoat/Pieces", false, /\.png$/);
+const patrolBoat = requirePatrolBoat.keys().map(requirePatrolBoat);
+let patrolBoatIndex = 0
+
 export default class Player {
     constructor(name) {
         this.name = name
@@ -23,6 +49,13 @@ export default class Player {
 
                 if (square[0] !== null) {
                     gameSquare.classList.add("ship")
+                    console.log(square)
+                    gameSquare.classList.add(`${square.name}`)
+                    let image = new Image()
+                    image.src = carrier
+                    console.log(image, gameSquare)
+
+                    this.renderShipImagesOntoDom(gameSquare)
                 }
                 gameSquare.setAttribute('x', x)
                 gameSquare.setAttribute('y', y)
@@ -39,6 +72,40 @@ export default class Player {
         let container = document.createElement('div');
         container.classList.add(`${name}`)
         return container
+    }
+
+    renderShipImagesOntoDom(element) {
+        let image_to_add = null;
+
+        if (element.classList.contains('Carrier')) {
+            image_to_add = carrier[carrierIndex];
+            carrierIndex = (carrierIndex + 1) % carrier.length;
+        }
+        else if (element.classList.contains('Battleship')) {
+            image_to_add = battleship[battleshipIndex];
+            battleshipIndex = (battleshipIndex + 1) % battleship.length;
+            
+        }
+        else if (element.classList.contains('Destroyer')) {
+            image_to_add = destroyer[destroyerIndex];
+            destroyerIndex = (destroyerIndex + 1) % destroyer.length;
+        }
+        else if (element.classList.contains('Submarine')) {
+            image_to_add = submarine[submarineIndex];
+            submarineIndex = (submarineIndex + 1) % submarine.length;
+            
+        }
+        else if (element.classList.contains('PatrolBoat')) {
+            image_to_add = patrolBoat[patrolBoatIndex];
+            patrolBoatIndex = (patrolBoatIndex + 1) % patrolBoat.length;
+        }
+
+        if (image_to_add){
+        element.style.backgroundImage = `url(${image_to_add})`;
+        element.style.backgroundRepeat = "no-repeat";
+        element.style.backgroundSize = "90% 100%";
+        element.style.backgroundPosition = "center";}
+        return element
     }
 
     renderBoard() {
