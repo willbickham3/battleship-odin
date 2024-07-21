c0b25a5fa908d1df288940e08de75457d73af847
Will <willbickham3@gmail.com>
2024-07-21 16:44:58 -0500

    Add image loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,11 @@ module.exports = {
         {
             test: /\.css$/,
             use: ['style-loader', 'css-loader'],
-        }
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
     ]
     }
 }
