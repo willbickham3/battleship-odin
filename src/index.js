c6fa5840fe585ee67857d7fa958b522c31840d9c
Will <willbickham3@gmail.com>
2024-07-23 00:36:53 -0500

    Explore the new computerPlayer class

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import GameRules from "./pregame.js"
 import Game from "./game.js";
 import './style.css';
 import _ from 'lodash';
+import ComputerPlayer from "./computerPlayer.js";
 
 
 // const player1 = new Player("player1")
@@ -11,6 +12,19 @@ import _ from 'lodash';
 // computer.renderBoard()
 // player1.board.placeShip("Destroyer", 3, 5)
 // player1.renderBoard()
+
+// const compPlayer = new ComputerPlayer()
+// console.log(compPlayer)
+// let attack = compPlayer.removeAttackFromSet(0, 0)
+// console.log(attack)
+// let secondAttack = compPlayer.computerAttack()
+// console.log(secondAttack)
+// let Attack = compPlayer.computerAttack()
+// console.log(Attack)
+// Attack = compPlayer.computerAttack()
+// console.log(Attack)
+// Attack = compPlayer.computerAttack()
+// console.log(Attack)
 
 // game
 const pregame = new GameRules()
@@ -22,6 +36,7 @@ startbutton.addEventListener('click', () => {
     const game = new Game("player1", "computer");
     game.startGame()
 })
+
 // const game = new Game("player1", "computer");
 // game.startGame()
 
