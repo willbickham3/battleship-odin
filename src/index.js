6031886f445dcb8163fc3e10a08549a90fab23e6
Will <willbickham3@gmail.com>
2024-07-23 22:47:25 -0500

    Add the ability to select a pvp game or computer game

--- src/index.js
+++ src/index.js
@@ -8,9 +8,23 @@ import ComputerPlayer from "./computerLogic/computerPlayer.js";
 const pregame = new GameRules()
 pregame.ruleContainer()
 
-let startbutton = document.querySelector('.startButton')
-startbutton.addEventListener('click', () => {
-    pregame.removeRuleSet()
-    const game = new Game("player1", "computer");
-    game.startGame()
+let startbutton = document.querySelectorAll('button');
+let player = null;
+startbutton.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.classList.contains("computer")) {
+            player = "computer"
+        }
+        else {
+            player = "player2"
+        }
+        pregame.removeRuleSet()
+        const game = new Game("player1", `${player}`);
+        game.startGame()
+    })
 })
+// startbutton.addEventListener('click', () => {
+//     pregame.removeRuleSet()
+//     const game = new Game("player1", "computer");
+//     game.startGame()
+// })
