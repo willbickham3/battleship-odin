04025a98d07a3e373bbaaf13c02b57dc97e0e202
Will <willbickham3@gmail.com>
2024-07-23 21:11:41 -0500

    Remove extraneous code

--- src/index.js
+++ src/index.js
@@ -4,28 +4,6 @@ import './style.css';
 import _ from 'lodash';
 import ComputerPlayer from "./computerLogic/computerPlayer.js";
 
-
-// const player1 = new Player("player1")
-// const computer = new Player('computer')
-
-// player1.renderBoard()
-// computer.renderBoard()
-// player1.board.placeShip("Destroyer", 3, 5)
-// player1.renderBoard()
-
-// const compPlayer = new ComputerPlayer()
-// console.log(compPlayer)
-// let attack = compPlayer.removeAttackFromSet(0, 0)
-// console.log(attack)
-// let secondAttack = compPlayer.computerAttack()
-// console.log(secondAttack)
-// let Attack = compPlayer.computerAttack()
-// console.log(Attack)
-// Attack = compPlayer.computerAttack()
-// console.log(Attack)
-// Attack = compPlayer.computerAttack()
-// console.log(Attack)
-
 // game
 const pregame = new GameRules()
 pregame.ruleContainer()
@@ -36,9 +14,3 @@ startbutton.addEventListener('click', () => {
     const game = new Game("player1", "computer");
     game.startGame()
 })
-
-// const game = new Game("player1", "computer");
-// game.startGame()
-
-// const game = new Game("Me", "Computer")
-// game.startGame()
