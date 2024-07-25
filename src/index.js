b133071de4c34b498f98d849799383878d52e5f2
Will <willbickham3@gmail.com>
2024-07-25 10:43:30 -0500

    Work on moving the player object out of game.js and into the main flow

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import Game from "./gameStates/game.js";
 import './style.css';
 import _ from 'lodash';
 import ComputerPlayer from "./computerLogic/computerPlayer.js";
+import Player from "./boardAssets/player.js";
 
 // game
 const pregame = new GameRules()
 pregame.ruleContainer()
+
+const player1    = new Player('player1')
+const compPlayer = new Player('computer')
 
 let startbutton = document.querySelectorAll('button');
 let player = null;
