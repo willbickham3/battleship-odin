56273f04be9a2486ca9db73e80ea9047e3b4930d
Will <willbickham3@gmail.com>
2024-07-25 23:50:50 -0500

    New folder to store audio files and audio JS files

---
+++ src/audio/audioSetup.js
@@ -0,0 +1,61 @@
+export default class AudioSetup {
+    constructor(audio) {
+        this.audioFile = new Audio(audio);
+        this.play = this.play.bind(this)
+        this.pause = this.pause.bind(this)
+    }
+
+    play() {
+        this.audioFile.play()
+    }
+
+    pause() {
+        this.audioFile.pause()
+    }
+
+    appendAudio() {
+        const header = document.querySelector('header');
+        const playBtn = this.musicButton('play');
+        const pauseBtn = this.musicButton('pause');
+
+        const volumeSlider = document.createElement('input');
+        volumeSlider.classList.add('volume-slider')
+        volumeSlider.setAttribute('type', "range")
+        volumeSlider.setAttribute('min', "0")
+        volumeSlider.setAttribute('max', "1")
+        volumeSlider.setAttribute('value', "50")
+        volumeSlider.setAttribute('step', "0.01")
+        volumeSlider.setAttribute('value', "0.5")
+
+        playBtn.addEventListener('click', () => {
+            this.audioFile.play()
+        })
+        pauseBtn.addEventListener('click', () => {
+            this.audioFile.pause()
+        })
+
+        volumeSlider.addEventListener('input', () => {
+            this.audioFile.volume = volumeSlider.value
+        })
+
+
+        header.append(playBtn, pauseBtn, volumeSlider)
+        header.append(this.audioFile)
+    }
+
+    audioContainer() {
+        const container = document.createElement('div').classList.add('audio-container');
+        return container
+    }
+
+    musicButton(className) {
+        const btn = document.createElement('button');
+        btn.style.width = '35px'
+        btn.style.height = '35px'
+        btn.classList.add(`${className}`)
+        return btn
+    }
+
+    volumeSlider() {
+    }
+}
---
+++ src/audio/pregameAudio.js
@@ -0,0 +1,5 @@
+import openingTheme from './sounds/ocean_sound_main_screen.wav'
+import AudioSetup from './audioSetup'
+
+const menuMusic = new AudioSetup(openingTheme)
+export default menuMusic
