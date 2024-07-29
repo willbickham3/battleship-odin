89e660987cdf525330688e41e539325906b24987
Will <willbickham3@gmail.com>
2024-07-29 00:52:35 -0500

    File handles weather changing to storm

---
+++ src/htmlAssets/weatherChange.js
@@ -0,0 +1,83 @@
+import { createH1, createDiv, createInput, createLabel } from "./util";
+import staticRain from '../images/staticRain.png'
+import rain from '../images/animatedRain.gif'
+import sunny from '../images/Sprite-0004.png'
+import AudioSetup from "../audio/audioSetup";
+
+export default class Weather {
+    createWeatherOptions() {
+        const weatherContainer = createDiv('weatherOptions');
+        weatherContainer.innerHTML = `<b>Weather Options</b>`
+        const stormOptions = this.stormSettings();
+        weatherContainer.append(stormOptions);
+        return weatherContainer
+    }
+
+    stormSettings() {
+        const stormContainer = createDiv('stormSettings');
+
+        const stormLabel = createLabel('storm');
+        stormLabel.innerText = 'Storm'
+        const stormInput = createInput('checkbox', 'storm', 'storm', null)
+
+        const staticStormLabel = createLabel('staticStorm');
+        staticStormLabel.innerText = 'Static Storm';
+        const staticStormInput = createInput('checkbox', 'staticStorm', 'staticStorm', null);
+
+        stormInput.addEventListener('input', () => {
+            const wavesAudio = document.querySelector('#menuMusic');
+            const stormAudio = document.querySelector('#stormAudio');
+            let volumeSliderValue = document.querySelector('#volume').getAttribute('value');
+            console.log(volumeSliderValue)
+            const body = document.querySelector('body');
+
+            if (stormInput.checked) {
+                if (!staticStormInput.checked) {
+                body.style.backgroundImage = `url(${rain})`}
+                else {
+                    body.style.backgroundImage = `url(${staticRain})`
+                }
+                wavesAudio.pause()
+                stormAudio.play()
+                if (stormAudio.getAttribute('prevVolume')) {
+                    stormAudio.volume = stormAudio.getAttribute('prevVolume');
+                }
+                else {
+                    stormAudio.volume = 0.2;
+                }
+            
+            }
+            else {
+                stormAudio.pause()
+                wavesAudio.play()
+                if (wavesAudio.getAttribute('prevVolume')) {
+                    wavesAudio.volume = wavesAudio.getAttribute('prevVolume');
+                }
+                else {
+                    wavesAudio.volume = 0.2;
+                }
+                body.style.backgroundImage = `url(${sunny})`
+            }
+        })
+
+        staticStormInput.addEventListener('input', () => {
+            const body = document.querySelector('body');
+            if (stormInput.checked && staticStormInput.checked) {
+                body.style.backgroundImage = `url(${staticRain})`
+            }
+            else if (stormInput.checked) {
+                body.style.backgroundImage = `url(${rain})`
+            }
+        })
+
+        const animStormContainer = createDiv('stormContainer');
+        const staticStormContainer = createDiv('staticStormContainer');
+        stormContainer.append(stormLabel, stormInput)
+        staticStormContainer.append(staticStormLabel, staticStormInput)
+
+        stormContainer.append(animStormContainer, staticStormContainer)
+        return stormContainer
+    }  
+
+
+}
