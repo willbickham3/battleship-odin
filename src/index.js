d0d4d978ef1e63c0711b23108d575c835c04dd14
Will <willbickham3@gmail.com>
2024-07-29 01:41:40 -0500

    Remove computerPlayer import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import GameRules from "./gameStates/pregame.js"
 import Game from "./gameStates/game.js";
 import './style.css';
 import _ from 'lodash';
-import ComputerPlayer from "./computerLogic/computerPlayer.js";
 import Player from "./gameLogic/player.js";
 import Header from "./htmlAssets/header.js";
 import AudioSetup from "./audio/audioSetup.js";
