9fc6da5c6ae4fcdc29c34a1e1cb0ee38736dd5b3
Will <willbickham3@gmail.com>
2024-07-29 00:52:52 -0500

    Stored html create methods

---
+++ src/htmlAssets/util.js
@@ -0,0 +1,31 @@
+function createH1(text) {
+    const h1 = document.createElement('h1');
+    h1.innerText = text
+}
+
+function createDiv(className) {
+    const div = document.createElement('div');
+    div.classList.add(`${className}`)
+    return div
+}
+
+function createLabel(labelPurpose) {
+    const label = document.createElement('label');
+    label.setAttribute('for', `${labelPurpose}`)
+    return label
+}
+
+function createInput(type, id, name, value, min = null, max = null) {
+    const input = document.createElement('input');
+    input.setAttribute("type", `${type}`)
+    input.setAttribute("id", `${id}`)
+    input.setAttribute("name", `${name}`)
+    input.setAttribute("value", `${value}`)
+    if (min !== null && max !== null) {
+    input.setAttribute("min", `${min}`)
+    input.setAttribute("max", `${max}`)
+    }
+    return input
+}
+
+export {createH1, createDiv, createLabel, createInput}
